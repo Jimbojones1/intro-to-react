1592febdb5c5b0d0181e35556a94172fff337631
JimHaff <james.haff@generalassemb.ly>
2024-03-04 12:41:47 -0600

    added todo

---
+++ src/ToDoList/ToDoList.jsx
@@ -0,0 +1,14 @@
+// Defining and exporting a component from this file
+// to be rendered by another component
+
+// ALL components must capitalized!
+export default function ToDoList() {
+  // The point of our components
+  // is to return some UI (what the user sees on the screen)
+  return (
+    <ul>
+      <li>Learn React</li>
+      <li>Learn the Mern Stack</li>
+    </ul>
+  );
+}
