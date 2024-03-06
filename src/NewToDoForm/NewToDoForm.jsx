7af7294bfb8422105ea614b2eceb336cd9307f88
JimHaff <james.haff@generalassemb.ly>
2024-03-06 12:25:57 -0600

    Added code

---
+++ src/NewToDoForm/NewToDoForm.jsx
@@ -0,0 +1,6 @@
+
+export default function NewToDoForm(){
+	return (
+		<h1>THis is the new to do form</h1>
+	)
+}
--- src/ToDoListItem/ToDoListItem.jsx
+++ src/ToDoListItem/ToDoListItem.jsx
@@ -19,3 +19,4 @@ export default function ToDoListItem({todo, index}){
 		</li>
 	)
 }
+
